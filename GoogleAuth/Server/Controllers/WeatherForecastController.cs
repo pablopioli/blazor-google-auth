1a72509778f80d75839251a2e46858504d63eaf7
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-09-11 11:24:14 -0300

    Starter version

---
+++ GoogleAuth/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,63 @@
+﻿using GoogleAuth.Shared;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleAuth.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        // The Web API will only accept tokens 1) for users, and 2) having the access_as_user scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "user_impersonation" };
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            Console.WriteLine(User.Identity.IsAuthenticated);
+
+            foreach (var userClaim in User.Claims)
+            {
+                Console.WriteLine($"{userClaim.Type}: {userClaim.Value}");
+            }
+
+            // get email
+            var claim = User.Claims.First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+            Console.WriteLine("Your google email es " + claim.Value);
+
+
+            // For this to work you need to request any additional scope 
+            // Otherwise you only gets an identity token
+            // In startup cs: options.Scope.Add("https://www.googleapis.com/auth/gmail.labels");
+            var token = await HttpContext.GetUserAccessTokenAsync();
+            Console.WriteLine(token);
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ GoogleAuth/Server/Startup.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GoogleAuth.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Read this from secure configuration
+            var clientId = "xxx";
+            var clientSecret = "xxx";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "Google";
+            })
+                  .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                  .AddGoogle("Google", options =>
+                        {
+                            options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                            options.ClientId = clientId;
+                            options.ClientSecret = clientSecret;
+
+                            // Need access to gmail labels? See scopes in https://developers.google.com/identity/protocols/oauth2/scopes
+                            options.Scope.Add("https://www.googleapis.com/auth/gmail.labels");
+
+                            // Use this if you need an ACCESS token
+                            // If you need only the identity remove this line, it will make your cookie smaller
+                            options.SaveTokens = true;
+                        });
+
+            services.AddAccessTokenManagement();
+
+            services.AddAuthorization(options =>
+            {
+                var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    "Google");
+
+                defaultAuthorizationPolicyBuilder =
+                    defaultAuthorizationPolicyBuilder
+                    .RequireAuthenticatedUser();
+
+                options.FallbackPolicy = defaultAuthorizationPolicyBuilder.Build();
+            });
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages().RequireAuthorization();
+                endpoints.MapControllers().RequireAuthorization();
+                endpoints.MapFallbackToFile("index.html").RequireAuthorization();
+            });
+        }
+    }
+}
